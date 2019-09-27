a19f6813ecec5df8d06dda453e2d41c32c01cba7
Kamila Zhakupova <zhakupova.kamila@gmail.com>
2019-09-27 14:40:41 +0600

    feat: добавлен класс Magnitude в Earthquake

---
+++ Earthquake/Magnitude.cs
@@ -0,0 +1,15 @@
+﻿namespace Earthquake
+{
+    public class Magnitude : IAttribute
+    {
+        public string Datasource { get; set; }
+        public string Eventsource { get; set; }
+        public string EventId { get; set; }
+        public string PublicId { get; set; }
+        public double MagnitudeValue { get; set; }
+        public string Type { get; set; }
+        public string OriginId { get; set; }
+        public string EvaluationMode { get; set; }
+        public CreationInfo CreationInfo { get; set; }
+    }
+}
