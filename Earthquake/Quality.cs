6592370d05912e7df2edf05fbb57c9a1f457dccb
Kamila Zhakupova <zhakupova.kamila@gmail.com>
2019-09-27 14:40:09 +0600

    feat: добавлен класс Quality в Earthquake

---
+++ Earthquake/Quality.cs
@@ -0,0 +1,10 @@
+﻿namespace Earthquake
+{
+    public class Quality
+    {
+        public int UsedPhaseCount { get; set; }
+        public double StandartError { get; set; }
+        public int AzimuthalGap { get; set; }
+        public double MinimumDistance { get; set; }
+    }
+}
