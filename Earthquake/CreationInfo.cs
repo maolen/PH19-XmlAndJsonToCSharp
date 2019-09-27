d970d96be48054a089f16b5c781e8e1cacb0db5f
Kamila Zhakupova <zhakupova.kamila@gmail.com>
2019-09-27 14:36:05 +0600

    feat: добавлен класс CreationInfo в Earthquake

---
+++ Earthquake/CreationInfo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Earthquake
+{
+    public class CreationInfo
+    {
+        public string AgencyId { get; set; }
+        public DateTime CreationTime { get; set; }
+    }
+}
