7411788ead9e7a19d8b07c39aa7e0a58d5525dde
Kamila Zhakupova <zhakupova.kamila@gmail.com>
2019-09-27 14:32:43 +0600

    feat: добавлен класс EventParameters в Earthquake

---
+++ Earthquake/EventParameters.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Earthquake
+{
+    public class EventParameters
+    {
+        public string PublicId { get; set; }
+        public List<Event> Events { get; set; }
+        public CreationInfo CreationInfo {get; set;}
+    }
+}
