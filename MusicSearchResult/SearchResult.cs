6e598454756553f8780336c198a89005c7e1119d
Kamila Zhakupova <zhakupova.kamila@gmail.com>
2019-09-27 14:50:55 +0600

    feat: добален класс SearchResult в MusicSearchResult

---
+++ MusicSearchResult/SearchResult.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace MusicSearchResult
+{
+    public class SearchResult
+    {
+        public int ResultCount { get; set; }
+        public List<Result> Results { get; set; }
+    }
+}
