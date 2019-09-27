4aa253368154d7d5ff90d734f443639e2d00658a
Kamila Zhakupova <zhakupova.kamila@gmail.com>
2019-09-27 14:34:03 +0600

    feat: добавлен класс Event в Earthquake

---
+++ Earthquake/Event.cs
@@ -0,0 +1,16 @@
+﻿namespace Earthquake
+{
+    public class Event : IAttribute
+    {
+        public string Datasource { get; set; }
+        public string Eventsource { get; set; }
+        public string EventId { get; set; }
+        public string PublicId { get; set; }
+        public Description Description { get; set; }
+        public Origin Origin { get; set; }
+        public Magnitude Magnitude { get; set; }
+        public string PreferredOriginID { get; set; }
+        public string PreferredMagnitudeID { get; set; }
+        public CreationInfo CreationInfo { get; set; }
+    }
+}
