1ae71b02d0652111d3c8caa9d99af83b7cdfa389
Kamila Zhakupova <zhakupova.kamila@gmail.com>
2019-09-27 14:33:33 +0600

    feat: добавлен интерфейс IAttribute в Earthquake

---
+++ Earthquake/IAttribute.cs
@@ -0,0 +1,10 @@
+﻿namespace Earthquake
+{
+    interface IAttribute
+    {
+        string Datasource { get; set; }
+        string Eventsource { get; set; }
+        string EventId { get; set; }
+        string PublicId { get; set; }
+    }
+}
